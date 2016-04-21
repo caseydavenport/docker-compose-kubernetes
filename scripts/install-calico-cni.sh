7db4bde1093ef41b88e234abdede6d2720a55bb6
Casey D <casey.davenport@metaswitch.com>
2016-04-21 08:30:50 -0700

    Update Calico

--- scripts/install-calico-cni.sh
+++ scripts/install-calico-cni.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Versions of Calico to install.
+CALICOCTL_VERISON=v0.19.0
+CALICO_CNI_VERSION=v1.3.0
+
 # Create necessary directories.
 sudo mkdir -p /etc/cni/net.d
 sudo mkdir -p /opt/cni/bin
@@ -26,13 +30,13 @@ sudo mv 10-calico.conf /etc/cni/net.d
 
 # Download the CNI plugin.
 if [ ! -f /opt/cni/bin/calico ]; then
-	sudo wget -O /opt/cni/bin/calico https://github.com/projectcalico/calico-cni/releases/download/v1.0.0/calico
+	sudo wget -O /opt/cni/bin/calico https://github.com/projectcalico/calico-cni/releases/download/${CALICO_CNI_VERSION}/calico
 	sudo chmod +x /opt/cni/bin/calico
 fi
 
 # Download the calicoctl tool.
 if [ ! -f /usr/local/bin/calicoctl ]; then
-	sudo wget -O /usr/local/bin/calicoctl https://github.com/projectcalico/calico-containers/releases/download/v0.16.0/calicoctl
+	sudo wget -O /usr/local/bin/calicoctl https://github.com/projectcalico/calico-containers/releases/download/${CALICOCTL_VERISON}/calicoctl
 	sudo chmod +x /usr/local/bin/calicoctl
 fi
 
